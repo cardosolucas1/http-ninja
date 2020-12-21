065f6c2c42afd0d4089e98eccc1fa1d7527bcad8
Cardoso <lsc.cardosoo@gmail.com>
2020-12-21 11:05:12 -0300

    Adicionar imagens aos cards

--- src/StatusList/index.ts
+++ src/StatusList/index.ts
@@ -18,8 +18,8 @@ const StatusList: Status[] = [
   {
     code: '103',
     title: 'Early Hints',
-    description: 'O código de informação de status de resposta HTTP 103 Early Hints é destinado principalmente a ser usado com o cabeçalho <em>Link</em> para permitir ao agente do usuário começar pré-carregamento de recursos enquanto o servidor ainda está preparando uma resposta.',
-    image: '999.png',
+    description: 'O código de informação de status de resposta HTTP 103 Early Hints é destinado principalmente a ser usado com o cabeçalho "Link" para permitir ao agente do usuário começar pré-carregamento de recursos enquanto o servidor ainda está preparando uma resposta.',
+    image: '103.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/103'
   },
   
@@ -41,84 +41,84 @@ const StatusList: Status[] = [
     code: '202',
     title: 'Accepted',
     description: 'A requisição foi recebida mas nenhuma ação foi tomada sobre ela. Isto é uma requisição não-comprometedora, o que significa que não há nenhuma maneira no HTTP para enviar uma resposta assíncrona indicando o resultado do processamento da solicitação. Isto é indicado para casos onde outro processo ou servidor lida com a requisição, ou para processamento em lote.',
-    image: '999.png',
+    image: '202.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/202'
   },
   {
     code: '203',
     title: 'Non-Authoritative Information',
     description: 'Esse código de resposta significa que o conjunto de meta-informações retornadas não é o conjunto exato disponível no servidor de origem, mas coletado de uma cópia local ou de terceiros. Exceto essa condição, a resposta de 200 OK deve ser preferida em vez dessa resposta.',
-    image: '999.png',
+    image: '203.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/203'
   },
   {
     code: '204',
     title: 'No Content',
     description: 'Não há conteúdo para enviar para esta solicitação, mas os cabeçalhos podem ser úteis. O user-agent pode atualizar seus cabeçalhos em cache para este recurso com os novos.',
-    image: '999.png',
+    image: '204.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/204'
   },
   {
     code: '205',
     title: 'Reset Content',
-    description: 'Esta requisição é enviada após realizanda a solicitação para informar ao user agent redefinir a visualização do documento que enviou essa solicitação.',
-    image: '999.png',
-    source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/205'
+    description: 'O 205 Reset Content status de resposta HTTP informa ao cliente para reconfigurar a visualização do documento, por exemplo, para limpar o conteúdo de um formulário, reconfigurar um estado de tela ou para atualizar a IU.',
+    image: '205.png',
+    source: 'https://docs.w3cub.com/http/status/205'
   },
   {
     code: '206',
     title: 'Partial Content',
     description: 'Esta resposta é usada por causa do cabeçalho de intervalo enviado pelo cliente para separar o download em vários fluxos.',
-    image: '999.png',
+    image: '206.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/206'
   },
   {
     code: '207',
     title: 'Multi-Status (WebDAV)',
     description: 'Uma resposta Multi-Status transmite informações sobre vários recursos em situações em que vários códigos de status podem ser apropriados.',
-    image: '999.png',
+    image: '207.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/207'
   },
   {
     code: '208',
     title: 'Multi-Status (WebDAV)',
     description: 'Usado dentro de um elemento de resposta <dav:propstat> para evitar enumerar os membros internos de várias ligações à mesma coleção repetidamente.',
-    image: '999.png',
+    image: '208.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/208'
   },
   {
     code: '226',
-    title: 'Multi-Status (WebDAV)',
+    title: 'IM Used (HTTP Delta encoding)',
     description: 'O servidor cumpriu uma solicitação GET para o recurso e a resposta é uma representação do resultado de uma ou mais manipulações de instância aplicadas à instância atual.',
-    image: '999.png',
+    image: '226.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/226'
   },
   {
     code: '300',
     title: 'Multiple Choice',
     description: 'A requisição tem mais de uma resposta possível. User-agent ou o user deve escolher uma delas. Não há maneira padrão para escolher uma das respostas.',
-    image: '999.png',
+    image: '300.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/300'
   },
   {
     code: '301',
     title: 'Moved Permanently',
     description: 'Esse código de resposta significa que a URI do recurso requerido mudou. Provavelmente, a nova URI será especificada na resposta.',
-    image: '999.png',
+    image: '301.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/301'
   },
   {
     code: '302',
     title: 'Found',
     description: 'Esse código de resposta significa que a URI do recurso requerido foi mudada temporariamente. Novas mudanças na URI poderão ser feitas no futuro. Portanto, a mesma URI deve ser usada pelo cliente em requisições futuras.',
-    image: '999.png',
+    image: '302.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/302'
   },
   {
     code: '303',
     title: 'See Other',
     description: 'O servidor manda essa resposta para instruir ao cliente buscar o recurso requisitado em outra URI com uma requisição GET.',
-    image: '999.png',
+    image: '303.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/303'
   },
   {
@@ -139,42 +139,42 @@ const StatusList: Status[] = [
     code: '306',
     title: 'unused',
     description: 'Esse código de resposta não é mais utilizado, encontra-se reservado. Foi usado numa versão anterior da especificação HTTP 1.1.',
-    image: '999.png',
+    image: '306.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/306'
   },
   {
     code: '307',
     title: 'Temporary Redirect',
     description: 'O código de estado 307 Redirecionamento temporário indica que o recurso da requisição foi temporariamente alterado para a URL informada no cabeçalho Location.',
-    image: '999.png',
+    image: '307.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/307'
   },
   {
     code: '308',
     title: 'Permanent Redirect',
     description: 'Esse código significa que o recurso agora está permanentemente localizado em outra URI, especificada pelo cabeçalho de resposta Location. Tem a mesma semântica do código de resposta HTTP 301 Moved Permanently  com a exceção de que o user-agent não deve mudar o método HTTP utilizado: se um POST foi utilizado na primeira requisição, um POST deve ser utilizado na segunda.',
-    image: '999.png',
+    image: '308.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/308'
   },
   {
     code: '400',
     title: 'Bad Request',
     description: 'Essa resposta significa que o servidor não entendeu a requisição pois está com uma sintaxe inválida.',
-    image: '999.png',
+    image: '400.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/400'
   },
   {
     code: '401',
     title: 'Unauthorized',
     description: 'O código de resposta de status de erro do cliente HTTP 401 Unauthorized  indica que a solicitação não foi aplicada porque não possui credenciais de autenticação válidas para o recurso de destino.',
-    image: '999.png',
+    image: '401.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/401'
   },
   {
     code: '402',
     title: 'Payment Required ',
     description: 'Este código de resposta está reservado para uso futuro. O objetivo inicial da criação deste código era usá-lo para sistemas digitais de pagamento porém ele não está sendo usado atualmente.',
-    image: '999.png',
+    image: '402.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/402'
   },
   {
@@ -195,203 +195,203 @@ const StatusList: Status[] = [
     code: '405',
     title: 'Method Not Allowed',
     description: 'O método de solicitação é conhecido pelo servidor, mas foi desativado e não pode ser usado. Os dois métodos obrigatórios, GET e HEAD, nunca devem ser desabilitados e não devem retornar este código de erro.',
-    image: '999.png',
+    image: '405.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/405'
   },
   {
     code: '406',
     title: 'Not Acceptable',
     description: 'Essa resposta é enviada quando o servidor da Web após realizar a negociação de conteúdo orientada pelo servidor, não encontra nenhum conteúdo seguindo os critérios fornecidos pelo agente do usuário.',
-    image: '999.png',
+    image: '406.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/406'
   },
   {
     code: '407',
     title: 'Proxy Authentication Required',
     description: 'Semelhante ao 401 porem é necessário que a autenticação seja feita por um proxy.',
-    image: '999.png',
+    image: '407.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/407'
   },
   {
     code: '408',
     title: 'Request Timeout',
     description: 'Esta resposta é enviada por alguns servidores em uma conexão ociosa, mesmo sem qualquer requisição prévia pelo cliente. Ela significa que o servidor gostaria de derrubar esta conexão em desuso. Esta resposta é muito usada já que alguns navegadores, como Chrome, Firefox 27+, ou IE9, usam mecanismos HTTP de pré-conexão para acelerar a navegação. Note também que alguns servidores meramente derrubam a conexão sem enviar esta mensagem.',
-    image: '999.png',
+    image: '408.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/408'
   },
   {
     code: '409',
     title: 'Conflict',
     description: 'Esta resposta será enviada quando uma requisição conflitar com o estado atual do servidor.',
-    image: '999.png',
+    image: '409.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/409'
   },
   {
     code: '410',
     title: 'Gone',
     description: 'Esta resposta será enviada quando o conteúdo requisitado foi permanentemente deletado do servidor, sem nenhum endereço de redirecionamento. É experado que clientes removam seus caches e links para o recurso. A especificação HTTP espera que este código de status seja usado para "serviços promocionais de tempo limitado". APIs não devem se sentir obrigadas a indicar que recursos foram removidos com este código de status.',
-    image: '999.png',
+    image: '410.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/410'
   },
   {
     code: '411',
     title: 'Length Required',
     description: 'O servidor rejeitou a requisição porque o campo Content-Length do cabeçalho não está definido e o servidor o requer.',
-    image: '999.png',
+    image: '411.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/411'
   },
   {
     code: '412',
     title: 'Precondition Failed',
     description: 'O cliente indicou nos seus cabeçalhos pré-condições que o servidor não atende.',
-    image: '999.png',
+    image: '412.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/412'
   },
   {
     code: '413',
     title: 'Payload Too Large',
     description: 'A entidade requisição é maior do que os limites definidos pelo servidor; o servidor pode fechar a conexão ou retornar um campo de cabeçalho Retry-After.',
-    image: '999.png',
+    image: '413.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/413'
   },
   {
     code: '414',
     title: 'URI Too Long',
-    description: 'A entidade requisição é maior do que os limites definidos pelo servidor; o servidor pode fechar a conexão ou retornar um campo de cabeçalho Retry-After.',
-    image: '999.png',
+    description: 'O código de resposta 414 URI Too Long indica que o tamanho da URI requisitada pelo cliente é maior do que o tamanho que o servidor aceita interpretar.',
+    image: '414.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/414'
   },
   {
     code: '415',
     title: 'Unsupported Media Type',
     description: 'O formato de mídia dos dados requisitados não é suportado pelo servidor, então o servidor rejeita a requisição.',
-    image: '999.png',
+    image: '415.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/415'
   },
   {
     code: '416',
     title: 'Requested Range Not Satisfiable',
     description: 'O formato de mídia dos dados requisitados não é suportado pelo servidor, então o servidor rejeita a requisição.',
-    image: '999.png',
+    image: '416.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/416'
   },
   {
     code: '417',
     title: 'Expectation Failed',
     description: 'Este código de resposta significa que a expectativa indicada pelo campo Expect do cabeçalho da requisição não pode ser satisfeita pelo servidor.',
-    image: '999.png',
+    image: '417.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/417'
   },
   {
     code: '418',
     title: 'I`m a teapot',
     description: 'O servidor recusa a tentativa de coar café num bule de chá.',
-    image: '999.png',
+    image: '418.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/418'
   },
   {
     code: '421',
     title: 'Misdirected Request',
     description: 'A requisição foi direcionada a um servidor inapto a produzir a resposta. Pode ser enviado por um servidor que não está configurado para produzir respostas para a combinação de esquema ("scheme") e autoridade inclusas na URI da requisição.',
-    image: '999.png',
+    image: '421.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/421'
   },
   {
     code: '422',
     title: 'Unprocessable Entity (WebDAV)',
     description: 'A requisição está bem formada mas inabilitada para ser seguida devido a erros semânticos.',
-    image: '999.png',
+    image: '422.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/422'
   },
   {
     code: '423',
     title: 'Locked (WebDAV)',
     description: 'O recurso sendo acessado está travado.',
-    image: '999.png',
+    image: '423.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/423'
   },
   {
     code: '424',
     title: 'Failed Dependency (WebDAV)',
     description: 'A requisição falhou devido a falha em requisição prévia.',
-    image: '999.png',
+    image: '424.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/424'
   },
   {
     code: '425',
     title: 'Too Early',
     description: 'Indica que o servidor não está disposto a arriscar processar uma requisição que pode ser refeita.',
-    image: '999.png',
+    image: '425.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/425'
   },
   {
     code: '426',
     title: 'Upgrade Required',
     description: 'O servidor se recusa a executar a requisição usando o protocolo corrente mas estará pronto a fazê-lo após o cliente atualizar para um protocolo diferente. O servidor envia um cabeçalho Upgrade numa resposta 426 para indicar o(s) protocolo(s) requeridos.',
-    image: '999.png',
+    image: '426.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/426'
   },
   {
     code: '428',
     title: 'Precondition Required',
     description: 'O servidor de origem requer que a resposta seja condicional. Feito para prevenir o problema da `atualização perdida`, onde um cliente pega o estado de um recurso (GET) , modifica-o, e o põe de volta no servidor (PUT), enquanto um terceiro modificou o estado no servidor, levando a um conflito.',
-    image: '999.png',
+    image: '428.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/426'
   },
   {
     code: '429',
     title: 'Too Many Requests',
     description: 'O usuário enviou muitas requisições num dado tempo ("limitação de frequência").',
-    image: '999.png',
+    image: '429.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/429'
   },
   {
     code: '431',
     title: 'Request Header Fields Too Large',
     description: 'O servidor não quer processar a requisição porque os campos de cabeçalho são muito grandes. A requisição PODE ser submetida novemente depois de reduzir o tamanho dos campos de cabeçalho.',
-    image: '999.png',
+    image: '431.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/431'
   },
   {
     code: '451',
     title: 'Unavailable For Legal Reasons',
     description: 'O usuário requisitou um recurso ilegal, tal como uma página censurada por um governo.',
-    image: '999.png',
+    image: '451.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/451'
   },
   {
     code: '500',
     title: 'Internal Server Error',
     description: 'O servidor encontrou uma situação com a qual não sabe lidar.',
-    image: '999.png',
+    image: '500.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/500'
   },
   {
     code: '501',
     title: 'Not Implemented',
     description: 'O método da requisição não é suportado pelo servidor e não pode ser manipulado. Os únicos métodos exigidos que servidores suportem (e portanto não devem retornar este código) são GET e HEAD.',
-    image: '999.png',
+    image: '501.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/500'
   },
   {
     code: '502',
     title: 'Bad Gateway',
     description: 'Esta resposta de erro significa que o servidor, ao trabalhar como um gateway a fim de obter uma resposta necessária para manipular a requisição, obteve uma resposta inválida.',
-    image: '999.png',
+    image: '502.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/502'
   },
   {
     code: '503',
     title: 'Service Unavailable',
     description: 'O servidor não está pronto para manipular a requisição. Causas comuns são um servidor em manutenção ou sobrecarregado. Note que junto a esta resposta, uma página amigável explicando o problema deveria ser enviada. Estas respostas devem ser usadas para condições temporárias e o cabeçalho HTTP Retry-After: deverá, se posível, conter o tempo estimado para recuperação do serviço. O webmaster deve também tomar cuidado com os cabaçalhos relacionados com o cache que são enviados com esta resposta, já que estas respostas de condições temporárias normalmente não deveriam ser postas em cache.',
-    image: '999.png',
+    image: '503.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/503'
   },
   {
     code: '504',
     title: 'Gateway Timeout',
     description: 'Esta resposta de erro é dada quando o servidor está atuando como um gateway e não obtém uma resposta a tempo.',
-    image: '999.png',
+    image: '504.png',
     source: 'https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/504'
   },
   {
