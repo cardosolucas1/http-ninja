fe098d63431aa7700161a2b0d1d8e573e5763ad7
Cardoso <lsc.cardosoo@gmail.com>
2020-12-22 11:00:30 -0300

    Implementar funcao para filtragem de codigos http

--- src/Components/Filter/index.tsx
+++ src/Components/Filter/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 interface Props {
   label: string;
+  handleFilter: Function,
 };
 
-const Filter: React.FC<Props> = ({ label }) => {
+
+const Filter: React.FC<Props> = ({ label, handleFilter }) => {
+  const [input, setInput] = useState<String>('');
+
+  useEffect(() => {
+    handleFilter(input);
+  }, [input])
+
   return (
     <div className="row filter">
     <form className="col s6">
       <div className="row">
         <div className="input-field col s12">
-            <textarea id="textarea1" className="materialize-textarea" maxLength={3}/>
+            <textarea
+              className="materialize-textarea"
+              id="textarea1"
+              maxLength={3}
+              onChange={(e) => setInput(e.target.value)}
+            />
             <label htmlFor="textarea1">{label}</label>
         </div>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StatusProvider from './Context/statusProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StatusProvider>
+      <App />
+    </StatusProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
